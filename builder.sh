188d875cc4508e7ab8c3df16dfbcb34a5e81a2b7
ExplosG <joel.wejdenstal@gmail.com>
2018-01-03 14:42:53 +0100

    Added core project files

---
+++ builder.sh
@@ -0,0 +1,17 @@
+echo 'Skript-EssentialsY' > essentialsy.sk
+echo '' >> essentialsy.sk
+echo '' >> essentialsy.sk
+cat src/functions.src >> essentialsy.sk
+echo '' >> essentialsy.sk
+echo '' >> essentialsy.sk
+cat src/triggers.src >> essentialsy.sk
+echo '' >> essentialsy.sk
+echo '' >> essentialsy.sk
+cat src/commands.src >> essentialsy.sk
+echo '' >> essentialsy.sk
+echo '' >> essentialsy.sk
+cat src/main.src >> essentialsy.sk
+echo '' >> essentialsy.sk
+echo '' >> essentialsy.sk
+echo Build finished
+read null
