21bd2782ff546ecc1f1790ce09bf4148397525ef
ExplosG <joel.wejdenstal@gmail.com>
2018-01-05 18:16:10 +0100

    Updated builder.sh to be more verbose

--- builder.sh
+++ builder.sh
@@ -1,14 +1,18 @@
 echo '# Skript-EssentialsY' > essentialsy.sk
 echo '' >> essentialsy.sk
 echo '' >> essentialsy.sk
+echo "Adding functions"
 cat src/functions.src >> essentialsy.sk
 echo '' >> essentialsy.sk
 echo '' >> essentialsy.sk
+echo "Adding triggers"
 cat src/triggers.src >> essentialsy.sk
 echo '' >> essentialsy.sk
 echo '' >> essentialsy.sk
+echo "Adding commands"
 cat src/commands.src >> essentialsy.sk
 echo '' >> essentialsy.sk
 echo '' >> essentialsy.sk
+echo "Adding main"
 cat src/main.src >> essentialsy.sk
 echo Build finished
