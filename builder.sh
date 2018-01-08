0f923f39a7fa4784ab4757d5ee65395e8feef430
2legal2 <joel.wejdenstal@gmail.com>
2018-01-08 19:24:01 +0100

    Optimized builder to remove newlines and comments to make the skript more compact

--- builder.sh
+++ builder.sh
@@ -1,18 +1,9 @@
-echo '# Skript-EssentialsY' > essentialsy.sk
-echo '' >> essentialsy.sk
-echo '' >> essentialsy.sk
-echo "Adding functions"
-cat src/functions.src >> essentialsy.sk
-echo '' >> essentialsy.sk
-echo '' >> essentialsy.sk
+echo "Adding functions..."
+grep -o '^[^#]*' src/functions.src | awk 'NF' >> essentialsy.sk
 echo "Adding triggers"
-cat src/triggers.src >> essentialsy.sk
-echo '' >> essentialsy.sk
-echo '' >> essentialsy.sk
+grep -o '^[^#]*' src/triggers.src | awk 'NF' >> essentialsy.sk
 echo "Adding commands"
-cat src/commands.src >> essentialsy.sk
-echo '' >> essentialsy.sk
-echo '' >> essentialsy.sk
+grep -o '^[^#]*' src/commands.src | awk 'NF' >> essentialsy.sk
 echo "Adding main"
-cat src/main.src >> essentialsy.sk
+grep -o '^[^#]*' src/main.src | awk 'NF' >> essentialsy.sk
 echo Build finished
