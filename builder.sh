2a7778ff7c69bde72b3bdf903b24e2498bfeb7c8
2legal2 <joel.wejdenstal@gmail.com>
2018-01-09 17:08:22 +0100

    Prettified builder and made the send prefix an option

--- builder.sh
+++ builder.sh
@@ -1,12 +1,12 @@
 echo "Build of EssentialsY started..."
-echo "Adding options"
+echo "Pruning and adding options.src"
 grep -o '^[^#]*' src/options.src | awk 'NF' > essentialsy.sk
-echo "Adding functions..."
+echo "Pruning and adding functions.src"
 grep -o '^[^#]*' src/functions.src | awk 'NF' >> essentialsy.sk
-echo "Adding triggers"
+echo "Pruning and adding triggers.src"
 grep -o '^[^#]*' src/triggers.src | awk 'NF' >> essentialsy.sk
-echo "Adding commands"
+echo "Pruning and adding commands.src"
 grep -o '^[^#]*' src/commands.src | awk 'NF' >> essentialsy.sk
-echo "Adding main"
+echo "Pruning and adding main.src"
 grep -o '^[^#]*' src/main.src | awk 'NF' >> essentialsy.sk
 echo "Build finished"
